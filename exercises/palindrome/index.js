c53a1ce29ea305b96ff786c753ce484a20b289c8
Jyothish Jose <jyothish6190@gmail.com>
2018-04-15 18:09:34 +0530

    solution 2

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -8,9 +8,9 @@
 //   palindrome("abcdefg") === false
 
 function palindrome(str) {
-    const rev = str.split('').reverse().join('');
-
-    return rev === str;
+	return str.split('').every((char, index) => {
+		return char === str[str.length - index - 1];
+	});
 }
 
 module.exports = palindrome;
